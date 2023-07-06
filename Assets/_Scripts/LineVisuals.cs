6f077eed1e0a509d33fbb6abf767b8249a05005b
valerii.korniienko <vakor.dev@gmail.com>
2023-07-06 10:41:39 +0300

    Game Input + LineVisuals + PlayerVisuals

---
+++ Assets/_Scripts/LineVisuals.cs
@@ -0,0 +1,83 @@
+﻿#region
+
+using UnityEngine;
+
+#endregion
+
+namespace _Scripts
+{
+    [RequireComponent(typeof(LineRenderer))]
+    public class LineVisuals : MonoBehaviour
+    {
+        [SerializeField] [Range(0, 50)] private int segments = 50;
+
+        private LineRenderer _lineRenderer;
+
+        private void Awake()
+        {
+            _lineRenderer = GetComponent<LineRenderer>();
+        }
+
+        private void Start()
+        {
+            InitLine();
+            RenderLine(Player.Instance.JumpRadius);
+
+            Player.Instance.OnLineWidthChanged += PlayerOnLineWidthChanged;
+            Player.Instance.OnJumpStarted += PlayerOnJumpStarted;
+            Player.Instance.OnJumpFinished += PlayerOnJumpFinished;
+        }
+
+        private void PlayerOnJumpFinished()
+        {
+            Show();
+        }
+
+        private void PlayerOnJumpStarted()
+        {
+            Hide();
+        }
+
+        private void PlayerOnLineWidthChanged()
+        {
+            RenderLine(Player.Instance.JumpRadius);
+        }
+
+        private void InitLine()
+        {
+            _lineRenderer.positionCount = segments + 1;
+            _lineRenderer.useWorldSpace = false;
+            _lineRenderer.widthMultiplier = 0.1f;
+        }
+
+        private void RenderLine(float radius)
+        {
+            CreatePoints(radius);
+        }
+
+        private void CreatePoints(float radius)
+        {
+            float angle = 20f;
+
+            for (int i = 0; i < (segments + 1); i++)
+            {
+                var x = Mathf.Sin(Mathf.Deg2Rad * angle) * radius;
+                var y = Mathf.Cos(Mathf.Deg2Rad * angle) * radius;
+
+                _lineRenderer.SetPosition(i, new Vector3(x, 0, y));
+
+                angle += (360f / segments);
+            }
+        }
+
+        private void Show()
+        {
+            gameObject.SetActive(true);
+        }
+
+        private void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/_Scripts/Systems/GameInput.cs
@@ -0,0 +1,40 @@
+﻿#region
+
+using System;
+using _Scripts.Helpers;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+#endregion
+
+namespace _Scripts.Systems
+{
+    public sealed class GameInput : StaticInstance<GameInput>
+    {
+        private PlayerInputActions _playerInputActions;
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            _playerInputActions = new PlayerInputActions();
+
+            _playerInputActions.Player.Enable();
+            _playerInputActions.Player.Jump.performed += JumpOnPerformed;
+        }
+
+        private void JumpOnPerformed(InputAction.CallbackContext obj)
+        {
+            OnJump?.Invoke();
+        }
+
+        public Vector2 GetMovementVectorNormalized()
+        {
+            Vector2 inputVector = _playerInputActions.Player.Move.ReadValue<Vector2>();
+
+            return inputVector.normalized;
+        }
+
+        public event Action OnJump;
+    }
+}
